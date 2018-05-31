d7edfa1418672bd98ab5032560ac7b22ac5e02fa
jacko <firmin@et.esiea.fr>
2018-05-31 18:14:19 +0200

    main activity modifié

--- app/src/main/java/org/esiea/firmin_jacques_olivier_chamoinri_ifuja/comicsproject/MainActivity.java
+++ app/src/main/java/org/esiea/firmin_jacques_olivier_chamoinri_ifuja/comicsproject/MainActivity.java
@@ -1,16 +1,54 @@
 package org.esiea.firmin_jacques_olivier_chamoinri_ifuja.comicsproject;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String URL="https://comicvine.gamespot.com/api/volumes/?api_key=8687da03d4d13477b19303ae25f9c24c09b02467&format=json";
+
+        RequestQueue requestQueue=Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest=new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                            Log.e("Rest Response", response.toString()) ;
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                            Log.e("Rest Response", error.toString()) ;
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
+
+
     }
     public void page1(View view) {
         startActivity(new Intent(this, page_2.class));
